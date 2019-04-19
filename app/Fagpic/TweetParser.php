894554ac57da909f96d166f50290278217224738
vagrant <vagrant@localhost.localdomain>
2019-04-19 07:04:55 +0000

    Parse処理実装。

---
+++ app/Fagpic/TweetParser.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Fagpic;
+
+class TweetParser extends Parser
+{
+    private $urls = array();
+
+    public function getUrls()
+    {
+        return $this->urls;
+    }
+
+    /* GET search/tweets で取得した結果を渡す */
+    public function parse( $object )
+    {
+        echo( count($object->statuses));
+        foreach( $object->statuses as $status )
+        {
+            // tweet階層
+            $hashtags = array();
+            foreach( $status->entities->hashtags as $hashtag )
+            {
+                // hashtag階層
+                array_push($hashtags, $hashtag->text);
+            }
+
+            $urls = array();
+            if( array_key_exists( 'media', $status->entities ))
+            {
+                foreach( $status->entities->media as $media )
+                {
+                    // 画像url階層
+                    array_push( $this->urls, [$status->id => $media->media_url_https] );
+                    array_push( $urls, $media->media_url_https);
+                }
+            }
+            
+            $this->setData( [ 'id'    => $status->id,
+                            'text'  => $status->text,
+                            'name'  => $status->user->name,
+                            'screen_name'   => $status->user->screen_name,
+                            'created_at'    => $status->created_at,
+                            'favorite_count'=> $status->favorite_count,
+                            'retweet_count'   => $status->retweet_count,
+                            'hashtags'      => $hashtags,
+                            'urls'  => $urls ]);
+        }
+    }
+}
