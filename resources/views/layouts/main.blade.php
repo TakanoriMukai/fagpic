ff85eb1f5a8dba551092a4d17d54456b3fd5972c
vagrant <vagrant@localhost.localdomain>
2019-04-21 03:00:13 +0000

    honokaの格納場所を修正。main画面作成中。

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.base')
+@section('title','Fagpic')
+
+@section('header')
+<nav class="navbar">
+    <div  class="navbar-brand"><a href='/'>Fagpic</a></div>
+</nav>
+@endsection
+
+@section('content')
+
+@endsection
